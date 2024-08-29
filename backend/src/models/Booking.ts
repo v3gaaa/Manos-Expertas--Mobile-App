d80c485f5c40063180683b60778d5b65ff7f634f
AdelaSolorio <a01637205@tec.mx>
2024-08-29 08:45:08 -0600

    Slight update to the booking collection

--- backend/src/models/Booking.ts
+++ backend/src/models/Booking.ts
@@ -5,7 +5,8 @@ export interface IBooking extends Document {
     user: mongoose.Schema.Types.ObjectId;    // Reference to User
     startDate: Date;
     endDate: Date;
-    completed: Boolean;  
+    completed: Boolean; 
+    createdAt: Date; 
     updatedAt: Date;
 }
 
@@ -16,8 +17,8 @@ const BookingSchema: Schema = new mongoose.Schema({
     startDate: { type: Date, required: true },
     endDate: { type: Date, required: true },
     completed: { type: Boolean, required: true, default: false },
-    createdAt: { type: Date },
-    updatedAt: { type: Date },
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: { type: Date, default: Date.now },
 });
 
 // Export the Booking model
